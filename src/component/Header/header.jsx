0f1a2248799520ca3d5b99221c6d31f098ad900d
EndeBedore <mukhsimkhanovUsman@hotmail.com>
2025-07-19 17:50:28 +0500

    zoyirjonikini ozinikiga qob qoydim

--- src/component/Header/header.jsx
+++ src/component/Header/header.jsx
@@ -1,7 +1,7 @@
 
 import './header.css'
 import logo from '../../assets/img/logo.png'
-import hero from "../../assets/img/hero.png"
+
 
 
 
@@ -23,16 +23,7 @@ function Header() {
                     </nav>
                 </div>
             </header>
-            <section className='hero'>
-                <div className="container">
-                    <div className="textHero">
-                        <h1>Управление запасами розничного магазина</h1>
-                        <p>Использование сервиса 1С-Товары позволяет значительно упростить работу по формированию заявок поставщику.</p>
-                        <button>Заказать демонстрацию</button>
-                    </div>
-                    <img src={hero} alt="hero png" />
-                </div>
-            </section>
+
         </div>
     )
 }
--- src/component/Main/Section1/index.jsx
+++ src/component/Main/Section1/index.jsx
@@ -1,16 +1,26 @@
 
 
 
-
+import hero from "../../assets/img/hero.png"
+import "./sec1.css"
 
 
 
 
 
 function Section1() {
-    return(
+    return (
         <div>
-            Section1
+            <section className='hero'>
+                <div className="container">
+                    <div className="textHero">
+                        <h1>Управление запасами розничного магазина</h1>
+                        <p>Использование сервиса 1С-Товары позволяет значительно упростить работу по формированию заявок поставщику.</p>
+                        <button>Заказать демонстрацию</button>
+                    </div>
+                    <img src={hero} alt="hero png" />
+                </div>
+            </section>
         </div>
     )
 }
